233cc9b644f60fb313f4f5f52b2e2da7c58df1f4
Raf Dul <rafalduluk@gmail.com>
2020-10-09 10:29:10 +0200

    Improvement Find the correct article & add class active

--- js/script.js
+++ js/script.js
@@ -1,109 +1,107 @@
-'use strict';
+"use strict";
 
 function myBlog() {
+  // document.getElementById('test-button').addEventListener('click', function(){
+  //     const links = document.querySelectorAll('.titles a');
+  //     console.log('links:', links);
+  //   });
 
-    // document.getElementById('test-button').addEventListener('click', function(){
-    //     const links = document.querySelectorAll('.titles a');
-    //     console.log('links:', links);
-    //   });
+  // Wyświetlanie artykułu po kliknięciu
+  const titleClickHandler = function (event) {
+    console.log("Link was clicked!");
+    // console.log(event);
 
-    // Wyświetlanie artykułu po kliknięciu
-    const titleClickHandler = function(event){
-        console.log('Link was clicked!');
-        // console.log(event);
+    /* [DONE] remove class 'active' from all article links  */
+    const activeLinks = document.querySelectorAll(".titles a.active");
 
-
-        /* [DONE] remove class 'active' from all article links  */
-        const activeLinks = document.querySelectorAll('.titles a.active');
-
-        for(let activeLink of activeLinks){
-        activeLink.classList.remove('active');
-        }
-
-        /* [DONE] add class 'active' to the clicked link */
-        event.preventDefault();
-        const clickedElement = this;
-        clickedElement.classList.add('active'); //pętla nie jest potrzebna, bo odniesienie do pojedynczego elementu
-
-        console.log('clickedElement:', clickedElement);
-        console.log('clickedElement (with plus): ' + clickedElement); //console.log z + pokazuje cały adres elementu
-
-        /* [DONE] remove class 'active' from all articles */
-        const activeArticles = document.querySelectorAll('article.active'); //selektor po elemencie article i kalsie .active
-
-        for(let activeArticle of activeArticles){
-            activeArticle.classList.remove('active');
-        }
-
-        /* [DONE] get 'href' attribute from the clicked link */
-        const articleSelector = clickedElement.getAttribute("href");
-        // const articleSelector = clickedElement.getAttribute("href").substring(1); 
-        //dodałem substring czyli wyrzucenie # z nazwy w href
-        console.log(clickedElement.getAttribute("href"));
-        // console.log(clickedElement.getAttribute("href").substring(1));
-
-        /* [DONE] find the correct article using the selector (value of 'href' attribute) */
-        const targetArticle = document.querySelector(clickedElement.getAttribute("href"));
-        console.log(targetArticle);
-
-        // Alternatywna opcja z wykorzystaniem elementu zidentyfikowanego po id; 
-        // konieczna jest wtedy modyfikacja nazwy "href" poprzez .substring(1)
-        // const targetArticle = document.getElementById(clickedElement.getAttribute("href").substring(1));
-        // console.log(targetArticle);
-
-        /* [DONE] add class 'active' to the correct article */
-        const printedArticle = targetArticle.classList.add('active');
-    
+    for (let activeLink of activeLinks) {
+      activeLink.classList.remove("active");
     }
-    
-    const links = document.querySelectorAll('.titles a');
 
-    for(let link of links){
-        link.addEventListener('click', titleClickHandler);
+    /* [DONE] add class 'active' to the clicked link */
+    event.preventDefault();
+    const clickedElement = this;
+    clickedElement.classList.add("active"); //pętla nie jest potrzebna, bo odniesienie do pojedynczego elementu
+
+    console.log("clickedElement:", clickedElement);
+    console.log("clickedElement (with plus): " + clickedElement); //console.log z + pokazuje cały adres elementu
+
+    /* [DONE] remove class 'active' from all articles */
+    const activeArticles = document.querySelectorAll("article.active"); //selektor po elemencie article i kalsie .active
+
+    for (let activeArticle of activeArticles) {
+      activeArticle.classList.remove("active");
     }
-    
-    // Generowanie listy tytułów
-    const optArticleSelector = '.post';
-    const optTitleSelector = '.post-title';
-    const optTitleListSelector = '.titles';
 
-    function generateTitleLinks(){
-        console.log('Title Links generated!');
+    /* [DONE] get 'href' attribute from the clicked link */
+    const articleSelector = clickedElement.getAttribute("href");
+    // const articleSelector = clickedElement.getAttribute("href").substring(1);
+    //dodałem substring czyli wyrzucenie # z nazwy w href
+    console.log(clickedElement.getAttribute("href"));
+    // console.log(clickedElement.getAttribute("href").substring(1));
 
-        /* [IN PROGRESS] remove contents of titleList */
-        function clearTitleList() {
-            document.querySelector('.titles').innerHTML = ''; //działa też z textContent
-            console.log("clearTitleList made");
-        } 
-        clearTitleList();
-        
-        /* [IN PROGRESS] for each article */
-      
-          /* [IN PROGRESS] get the article id */
-        const articleSelectorId_s = document.querySelectorAll('.post');
+    /* [DONE] find the correct article using the selector (value of 'href' attribute) */
+    const targetArticle = document.querySelector(
+      clickedElement.getAttribute("href")
+    );
+    console.log(targetArticle);
 
-        for (let articleSelectorId of articleSelectorId_s) {
-            articleSelectorId.getAttribute("id");
-            // console.log(articleSelectorId.getAttribute("id"));
-        }
-      
-          /* find the title element */
-        const articleTitles = document.querySelectorAll('h3.post-title');
-        console.log(articleTitles);
+    // Alternatywna opcja z wykorzystaniem elementu zidentyfikowanego po id;
+    // konieczna jest wtedy modyfikacja nazwy "href" poprzez .substring(1)
+    // const targetArticle = document.getElementById(clickedElement.getAttribute("href").substring(1));
+    // console.log(targetArticle);
 
-        for (let articleTitle of articleTitles) {
-            articleTitle.innerHTML;
-            console.log(articleTitle.innerHTML);
-        }
-      
-          /* get the title from the title element */
-      
-          /* create HTML of the link */
-      
-          /* insert link into titleList */
-      
-    }
-    generateTitleLinks();
+    /* [DONE] add class 'active' to the correct article */
+    const printedArticle = targetArticle.classList.add("active");
+  };
 
+  const links = document.querySelectorAll(".titles a");
+
+  for (let link of links) {
+    link.addEventListener("click", titleClickHandler);
+  }
+
+  // // Generowanie listy tytułów
+  // const optArticleSelector = '.post';
+  // const optTitleSelector = '.post-title';
+  // const optTitleListSelector = '.titles';
+
+  // function generateTitleLinks(){
+  //     console.log('Title Links generated!');
+
+  //     /* [DONE] remove contents of titleList */
+  //     function clearTitleList() {
+  //         document.querySelector('.titles').innerHTML = ''; //działa też z textContent
+  //         console.log("clearTitleList made");
+  //     }
+  //     clearTitleList();
+
+  //     /* [IN PROGRESS] for each article */
+
+  //       /* [DONE] get the article id */
+  //     const articleSelectorId_s = document.querySelectorAll('.post');
+
+  //     for (let articleSelectorId of articleSelectorId_s) {
+  //         articleSelectorId.getAttribute("id");
+  //         // console.log(articleSelectorId.getAttribute("id"));
+  //     }
+
+  //       /* [DONE] find the title element */
+  //     const articleTitles = document.querySelectorAll('h3.post-title');
+  //     // console.log(articleTitles);
+
+  //     for (let articleTitle of articleTitles) {
+  //         articleTitle.innerHTML;
+  //         console.log(articleTitle.innerHTML);
+  //     }
+
+  //       /* [IN PROGRESS] get the title from the title element */
+
+  //       /* create HTML of the link */
+
+  //       /* insert link into titleList */
+
+  // }
+  // generateTitleLinks();
 }
 myBlog();
